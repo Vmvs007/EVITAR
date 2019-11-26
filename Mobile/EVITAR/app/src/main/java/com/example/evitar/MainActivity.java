1d988c3d39a9ac04a96f5da4db6dd8bc92f41ccb
8170262 <8170262@estg.ipp.pt>
2019-11-26 16:58:42 +0000

    ss

--- Mobile/EVITAR/app/src/main/java/com/example/evitar/MainActivity.java
+++ Mobile/EVITAR/app/src/main/java/com/example/evitar/MainActivity.java
@@ -4,29 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT=5000;
+    private static int SPLASH_TIME_OUT=3000;
     ImageView logo, girl, wave;
-    Animation bigtosmall;
+    TextView wtext;
+    Button skip;
+    Animation descer,rotation, walktext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
 
-        Button skip=(Button) findViewById(R.id.skip);
 
-        bigtosmall= AnimationUtils.loadAnimation(this, R.anim.bigtosmall);
+        descer= AnimationUtils.loadAnimation(this, R.anim.descer);
+        rotation= AnimationUtils.loadAnimation(this, R.anim.rotation);
+        walktext= AnimationUtils.loadAnimation(this, R.anim.walktext);
 
-        logo=(ImageView) findViewById(R.id.imageView2);
+        logo=(ImageView) findViewById(R.id.imageView);
 
-        logo.startAnimation(bigtosmall);
+        logo.startAnimation(rotation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -35,18 +40,23 @@ public class MainActivity extends AppCompatActivity {
                 logo=(ImageView) findViewById(R.id.imageView5);
                 girl=(ImageView) findViewById(R.id.imageView3);
                 wave=(ImageView) findViewById(R.id.imageView4);
+                wtext=(TextView) findViewById(R.id.textView);
+                skip=(Button) findViewById(R.id.skip);
 
-                girl.startAnimation(bigtosmall);
-                wave.startAnimation(bigtosmall);
-                logo.startAnimation(bigtosmall);
+                girl.startAnimation(descer);
+                wave.startAnimation(descer);
+                logo.startAnimation(descer);
+                wtext.startAnimation(walktext);
+                skip.startAnimation(walktext);
+
+                skip.setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View v) {
+                        setContentView(R.layout.login);
+                    }
+                });
             }
         },SPLASH_TIME_OUT);
-/*
-        skip.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-
-            }
-        });*/
     }
 }
