efce6f46f17bc4e804b03a929ea484d7f05f7dcd
8170262 <8170262@estg.ipp.pt>
2019-11-27 23:02:40 +0000

    toolbar

--- Mobile/EVITAR/app/src/main/java/com/example/evitar/MenuActivity.java
+++ Mobile/EVITAR/app/src/main/java/com/example/evitar/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.example.evitar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -19,6 +21,11 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        Toolbar myToolbar= (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+
 
         fragment=new HomeFragment();
 
@@ -76,5 +83,10 @@ public class MenuActivity extends AppCompatActivity {
 
 
 
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
