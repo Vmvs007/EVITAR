e7c934626d06f4353a8d301bda5004416339c62f
Dumbrica <Dumbrica@hotmail.com>
2019-12-23 19:05:16 +0000

    migrations

--- Backend/EvitarBackEnd/Models/EPIModel.cs
+++ Backend/EvitarBackEnd/Models/EPIModel.cs
@@ -12,15 +12,15 @@ namespace EvitarBackEnd.Models
         public string NomeEPI {get;set;}
         public DateTime DataRegistoEPI {get;set;}
         public DateTime DataValidadeEPI {get;set;}
-
+       
         public int Valido{get;set;}
+
+        public int IdColaborador {get;set;}
+        [System.ComponentModel.DataAnnotations.Schema.ForeignKey("IdColaborador")]
+        public ColaboradorModel IdColaboradorForeignKey {get;set;}
 
         public int IdTipoEPI {get;set;}
         [System.ComponentModel.DataAnnotations.Schema.ForeignKey("IdTipoEPI")]
         public TipoEPIModel IdTipoEPIForeignKey {get;set;}
-        
-        public int IdColaborador {get;set;}
-        [System.ComponentModel.DataAnnotations.Schema.ForeignKey("IdColaborador")]
-        public ColaboradorModel IdColaboradorForeignKey {get;set;}
     }
 }
